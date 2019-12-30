eaae2785432297f58502fd7fb62453c5db2f572c
Benjamin Kovach <bkovach13@gmail.com>
2019-12-29 21:08:05 -0500

    Add RGB functions to ChaosBox.Color

--- src/ChaosBox/Color.hs
+++ src/ChaosBox/Color.hs
@@ -1,8 +1,30 @@
-module ChaosBox.Color (HSV(..),WithAlpha(..),setSourceHSV,setSourceHSVA) where
+module ChaosBox.Color
+  (
+    -- * HSV Values (Hue-Saturation-Value)
+    HSV(..)
+  , WithAlpha(..)
+  , setSourceHSV
+  , setSourceHSVA
 
-import           Data.Colour.RGBSpace
+    -- * RGB Values (Red-Green-Blue)
+  , rgbFromHex
+  , setSourceRGB
+  , rgb255
+  , grayscale
+
+    -- * Conversion functions
+  , toHSV
+  , toRGB
+  , module X
+  )
+where
+
+import           Data.Colour.RGBSpace          as X
 import           Data.Colour.RGBSpace.HSV
 import           Graphics.Rendering.Cairo
+                                         hiding ( setSourceRGB )
+import qualified Graphics.Rendering.Cairo      as Cairo
+import           Data.Colour.SRGB
 
 data HSV = HSV
   { hsvHue        :: Double
@@ -16,11 +38,45 @@ data WithAlpha color = WithAlpha
   } deriving (Show, Read, Eq, Ord)
 
 setSourceHSV :: HSV -> Render ()
-setSourceHSV color = setSourceHSVA (color `WithAlpha` 1)
+setSourceHSV = setSourceRGB . toRGB
 
 setSourceHSVA :: WithAlpha HSV -> Render ()
 setSourceHSVA (WithAlpha HSV {..} alpha) =
   hsva hsvHue hsvSaturation hsvValue alpha
+
+rgbFromHex :: String -> RGB Double
+rgbFromHex = toSRGB . sRGB24read
+
+setSourceRGB :: RGB Double -> Render ()
+setSourceRGB (RGB r g b) = Cairo.setSourceRGB r g b
+
+rgb255 :: Fractional a => a -> a -> a -> RGB a
+rgb255 r g b = RGB (r / 255) (g / 255) (b / 255)
+
+toHSV :: RGB Double -> HSV
+toHSV rgb = let (h, s, v) = hsvView rgb in HSV h s v
+
+grayscale :: Num a => a -> RGB a
+grayscale value = RGB value value value
+
+toRGB :: HSV -> RGB Double
+toRGB HSV {..} = hsv2rgb hsvHue hsvSaturation hsvValue
+
+hsv2rgb :: RealFrac a => a -> a -> a -> RGB a
+hsv2rgb h s v = case i `mod` 6 of
+  0 -> RGB v t p
+  1 -> RGB q v p
+  2 -> RGB p v t
+  3 -> RGB p q v
+  4 -> RGB t p v
+  5 -> RGB v p q
+  _ -> error "mod 6 returned something out of range"
+ where
+  i = floor $ h * 6
+  f = h * 6 - fromIntegral i
+  p = v * (1 - s)
+  q = v * (1 - f * s)
+  t = v * (1 - (1 - f) * s)
 
 hsva :: Double -> Double -> Double -> Double -> Render ()
 hsva h s v = setSourceRGBA channelRed channelGreen channelBlue
