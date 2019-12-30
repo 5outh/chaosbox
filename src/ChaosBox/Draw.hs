d74f2bd0ccfb3813f99e544fc66370b528be724b
Benjamin Kovach <bkovach13@gmail.com>
2019-12-30 11:21:57 -0500

    Clarify docs

--- src/ChaosBox/Draw.hs
+++ src/ChaosBox/Draw.hs
@@ -7,6 +7,7 @@ import           Graphics.Rendering.Cairo
 
 -- | Class of drawable items.
 --
--- 'draw' should trace the path of a shape; nothing more.
+-- 'draw' traces the path of a shape, which can then be 'fill'ed or 'stroke'd
+-- using the regular cairo utilities.
 class Draw a where
   draw :: a -> Render ()
