4d355f64f121068c8bc91db0b97762b6d3801dfb
Benjamin Kovach <bkovach13@gmail.com>
2020-02-21 19:49:44 -0500

    WIPs

---
+++ src/ChaosBox/Geometry/Circle.hs
@@ -0,0 +1,8 @@
+module ChaosBox.Geometry.Circle
+  ()
+where
+
+-- TODO
+
+f :: _
+f = undefined
--- src/ChaosBox/Geometry/Path.hs
+++ src/ChaosBox/Geometry/Path.hs
@@ -1,7 +1,7 @@
 module ChaosBox.Geometry.Path
-  ( Path(Path)
-  , getPath
+  ( Path(..)
   , path
+  , bakePath
   )
 where
 
@@ -15,7 +15,7 @@ import qualified Data.List.NonEmpty       as NE
 import           Graphics.Rendering.Cairo hiding (Path)
 
 -- | An open path
-data Path = Path { _path :: NonEmpty (V2 Double), pathMatrix :: M33 Double }
+data Path = Path { getPath :: NonEmpty (V2 Double), pathMatrix :: M33 Double }
   deriving (Show, Eq, Ord)
 
 instance Affine Path where
@@ -27,13 +27,12 @@ instance Draw Path where
     moveTo startX startY
     for_ rest (\(V2 x y) -> lineTo x y)
 
--- NB. this operation and those like it are useful but should not be a part of
--- the public API.
-bakePath :: Path -> Path
-bakePath = withReset $ \p -> p { _path = fmap (applyAffine p) (_path p) }
-
-getPath :: Path -> NonEmpty (V2 Double)
-getPath = _path . bakePath
-
 path :: [V2 Double] -> Maybe Path
 path xs = Path <$> NE.nonEmpty xs <*> pure identity
+
+bakePath :: Path -> Path
+bakePath = withReset $ \p -> p { getPath = fmap (applyAffine p) (getPath p) }
+
+-- transformed -> deferred
+-- transform   -> multiply by transform and bake
+-- bake        -> apply transformation matrix
