c3911cfd222453dcca80f2e697d205480b26b962
Benjamin Kovach <bkovach13@gmail.com>
2020-02-26 16:17:01 -0500

    Add StdUniform for gamma distribution

--- src/ChaosBox/Orphanage.hs
+++ src/ChaosBox/Orphanage.hs
@@ -4,7 +4,7 @@ module ChaosBox.Orphanage
 where
 
 import           Data.Random (Distribution, Distribution (..), Normal,
-                              Normal (..))
+                              Normal (..), StdUniform (..))
 import           Linear.V2
 
 -- These orphan instances do not leak to the end-user, since they're only used
@@ -14,3 +14,6 @@ instance Distribution Normal a => Distribution Normal (V2 a) where
   rvarT StdNormal = V2 <$> rvarT StdNormal <*> rvarT StdNormal
   rvarT (Normal (V2 mx my) (V2 sx sy)) =
     V2 <$> rvarT (Normal mx sx) <*> rvarT (Normal my sy)
+
+instance Distribution StdUniform a => Distribution StdUniform (V2 a) where
+  rvarT StdUniform = V2 <$> rvarT StdUniform <*> rvarT StdUniform
