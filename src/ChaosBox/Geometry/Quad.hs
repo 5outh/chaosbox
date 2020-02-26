bfa2b949470c3fd526bbec8b053bb22bd7eb28de
Benjamin Kovach <bkovach13@gmail.com>
2020-02-26 11:05:33 -0500

    Minor changes

--- src/ChaosBox/Geometry/Quad.hs
+++ src/ChaosBox/Geometry/Quad.hs
@@ -7,18 +7,17 @@ where
 
 import           ChaosBox.Prelude
 
-import           ChaosBox.Math.Matrix           ( applyMatrix )
-import           ChaosBox.Geometry.Polygon      ( polygon )
 import           ChaosBox.Affine
 import           ChaosBox.Draw
+import           ChaosBox.Geometry.Polygon (polygon)
 import           ChaosBox.HasV2
-import           Data.Foldable                  ( for_ )
+import           Data.Foldable             (for_)
 
 data QuadOf a = QuadOf
-  { quadA      :: a
-  , quadB      :: a
-  , quadC      :: a
-  , quadD      :: a
+  { quadA :: a
+  , quadB :: a
+  , quadC :: a
+  , quadD :: a
   }
   deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable)
 
@@ -26,10 +25,9 @@ instance HasV2 a => Affine (QuadOf a) where
   transform = defaultTransform
 
 instance HasV2 a => Draw (QuadOf a) where
-  draw QuadOf {..} = for_ (polygon [quadA, quadB, quadC, quadD])
-    draw
+  draw QuadOf {..} = for_ (polygon [quadA, quadB, quadC, quadD]) draw
 
 type Quad = QuadOf (V2 Double)
 
 quad :: a -> a -> a -> a -> QuadOf a
-quad a b c d = QuadOf a b c d
+quad = QuadOf
