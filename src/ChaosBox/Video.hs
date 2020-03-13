34867bc8bf7b5ccdf3a91ee524fd3a309d495a5c
Benjamin Kovach <bkovach13@gmail.com>
2020-03-13 17:14:35 -0400

    wip behaviors

--- src/ChaosBox/Video.hs
+++ src/ChaosBox/Video.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE UndecidableInstances #-}
 module ChaosBox.Video
   ( renderFrame
   , eventLoop
@@ -12,22 +13,32 @@ module ChaosBox.Video
   , onKeyUp
   -- * Debugging
   , debugEvents
+  -- * Behavior combinators
+  , withBehavior
+  , withBehaviorM
   -- * Re-exports
   , MouseButton(..)
+  -- * Behaviors
+  , Behavior(..)
+  , behavior
+  , grab
   )
 where
 
 import           ChaosBox
 
-import           Control.Concurrent     (threadDelay)
-import           Control.Monad          (unless, void, when)
+import           Control.Concurrent            (threadDelay)
+import           Control.Monad                 (unless, void, when)
+import           Control.Monad.Base
 import           Control.Monad.IO.Class
-import           Control.Monad.Reader   (asks)
-import           Data.Foldable          (for_)
+import           Control.Monad.Random
+import           Control.Monad.Reader
+import           Data.Foldable                 (for_)
 import           Data.IORef.Lifted
 import qualified SDL
 import           SDL.Event
 import           System.CPUTime
+import           System.Random.Mersenne.Pure64
 
 -- | The main video rendering loop
 --
@@ -49,6 +60,7 @@ eventLoop act = loop
       renderFrame
       loop
 
+-- is *this* just a behavior???
 -- | Register a new event handler for an 'SDL.Event'
 registerEventHandler :: (SDL.Event -> Generate ()) -> Generate ()
 registerEventHandler handleEvent = do
@@ -59,6 +71,15 @@ registerEventHandler handleEvent = do
 -- | Print every 'SDL.Event' flowing through 'ChaosBox'
 debugEvents :: Generate ()
 debugEvents = registerEventHandler $ \event -> liftIO $ print event
+
+-- Note this doesn't actually *register* event handlers, it just encompasses a
+-- value whose value is tracked over time
+-- rawEvent
+  -- :: MonadBase IO m
+  -- => SDL.Event
+  -- -> Behavior (RandT PureMT (ReaderT GenerateCtx m)) SDL.Event
+  -- m ((a -> m a) -> m a)
+-- rawEvent = pure
 
 -- | Do something when a 'MouseButton' is initially 'Pressed'
 onMouseDown :: MouseButton -> (P2 -> Generate ()) -> Generate ()
@@ -109,6 +130,7 @@ onMouseMotion act = registerEventHandler $ \event -> case eventPayload event of
 --
 -- When the specified 'MouseButton' is held down, this returns 'Just mousePos'
 -- in user-space coordinates. Otherwise, it returns 'Nothing'.
+--
 syncHeldMousePosition :: MouseButton -> Generate (IORef (Maybe P2))
 syncHeldMousePosition button = do
   clickedPointRef <- newIORef Nothing
@@ -120,8 +142,132 @@ syncHeldMousePosition button = do
     for_ mPoint $ \_ -> writeIORef clickedPointRef (Just p)
   pure clickedPointRef
 
+-- TODO: Want some notion of "a variable that updates on given events"
+-- e.g. on tick => update an IORef
+--
+-- 'syncHeldMousePosition' is an example of this
+
+newtype Behavior m a = Behavior { runBehavior :: m ((a -> m a) -> m a) }
+
+-- I think the key here might be the Monad instance of Behavior (`bind`)
+-- clickedPoint <- behavior Nothing
+-- downPoint <- clickedPoint $ \mp -> case mp of
+--   Nothing -> pure Nothing
+--   Just a -> pure (Just a)
+--  upPoint <- downPoint $ \mp -> Nothing
+--
+
+-- (a -> m a) is an _event handler_, it describes how something changes
+
+getBehavior :: MonadBase IO m => Behavior m a -> m a
+getBehavior (Behavior ba) = do
+  fa <- ba
+  fa pure
+
+grab :: MonadBase IO m => Behavior m a -> m a
+grab = getBehavior
+
+behavior :: MonadBase IO m => a -> Behavior m a
+behavior a = Behavior $ do
+  a0 <- newIORef a
+  pure $ \f -> do
+    a1   <- readIORef a0
+    next <- f a1
+    next <$ writeIORef a0 next
+
+-- how do these compose, that is the question
+
+-- smash
+  -- :: MonadBase IO m
+  -- => (a -> b -> c)
+  -- -> Behavior m a -- Behavior a
+  -- -> Behavior m b -- Behavior b
+  -- -> Behavior m c -- Behavior c
+-- smash f ba bb = do
+  -- fa <- ba
+  -- fb <- bb
+  -- a  <- fa pure
+  -- b  <- fb pure
+  -- behavior (f a b)
+
+mapBehavior :: MonadBase IO m => (a -> b) -> Behavior m a -> Behavior m b
+mapBehavior f ba = Behavior $ do
+  a <- getBehavior ba
+  runBehavior (behavior (f a))
+
+instance MonadBase IO m => Functor (Behavior m) where
+  fmap = mapBehavior
+
+appBehavior
+  :: MonadBase IO m => Behavior m (a -> b) -> Behavior m a -> Behavior m b
+appBehavior fb ba = Behavior $ do
+  f <- getBehavior fb
+  a <- getBehavior ba
+  runBehavior (behavior (f a))
+
+instance MonadBase IO m => Applicative (Behavior m) where
+  pure  = behavior
+  (<*>) = appBehavior
+
+bindBehavior
+  :: MonadBase IO m => Behavior m a -> (a -> Behavior m b) -> Behavior m b
+bindBehavior ba f = Behavior $ do
+  a <- getBehavior ba
+  runBehavior (f a)
+
+instance MonadBase IO m => Monad (Behavior m) where
+  (>>=) = bindBehavior
+
+-- foo :: MonadBase IO m => Behavior m Int
+-- foo = do
+  -- n <- behavior 0
+  -- m <- behavior "100"
+  -- pure $ n + read m
+
+-- this is a composed behavior
+-- foo :: IO ((Int -> IO Int) -> IO Int)
+-- foo = do
+  -- n <- behavior 0
+  -- m <- behavior "100"
+  -- smash (\a b -> a + read b) (behavior 0) (behavior "100")
+
+-- compose :: Behavior a -> Behavior a -> Behavior a
+-- compose fa fb = do
+  -- n <- fa
+  -- m <- fb
+  -- pure $ \a -> do
+   -- b :: a <- n a
+   -- c :: a <- m b
+   -- m _c
+--
+-- n <- behavior 0
+-- m <- behavior 2
+-- -- would apply to BOTH n and m, then return the result
+-- t <- compose n m
+-- b <- (0 + 1) -- updates 'n'
+-- m (+1)       -- updates 'm'
+--
+-- -- result: both 'n' and 'm' get incremented by 1, t = n + m and is reactive
+--
+-- combine :: (a -> b -> c) -> Behavior a a -> Behavior d b -> Behavior a c
+--
+-- n <- behavior 0
+-- m <- behavior "100"
+-- res <- combine (\n0 m0 -> n0 + read m0) n m
+--
+-- res = 100
+--
+-- n (+1)
+--
+-- res = 101
+--
+
+
 onKeyUp :: SDL.Scancode -> Generate () -> Generate ()
 onKeyUp = undefined -- todo
+
+onKeyDown :: SDL.Scancode -> Generate () -> Generate ()
+onKeyDown = undefined -- todo
 
 -- | Render a frame one-off
 --
@@ -152,3 +298,13 @@ renderFrame = do
       threadDelay waitNs
       SDL.updateWindowSurface window
       writeIORef vmLastRenderedTimeRef now
+
+withBehavior :: MonadBase IO m => IORef t -> (t -> b) -> m b
+withBehavior b f = do
+  b0 <- readIORef b
+  pure (f b0)
+
+withBehaviorM :: MonadBase IO m => IORef t -> (t -> m b) -> m b
+withBehaviorM b f = do
+  b0 <- readIORef b
+  f b0
